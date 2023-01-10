8f3d064536668817d22e796754eb2217ffa077a9
Danil Makushev <reurairin@gmail.com>
2023-01-10 10:15:57 +0200

    add chat functionality

--- client/app.js
+++ client/app.js
@@ -6,18 +6,38 @@ let currentUrl = localUrl;
 const chatBox = document.getElementById('chat-box');
 const chatInput = document.getElementById('chat-input');
 
-function sendMessage() {
-    if (chatInput.value?.length > 0) {
+function sendMessage() {    
+    const userInput = chatInput.value;
+    addMessageToChat(userInput, 'client');
+    if (userInput?.length > 0) {
         fetch(currentUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ utterance: chatInput.value })
+            body: JSON.stringify({ utterance: userInput })
         })
             .then((res) => res.json())
             .then((res) => {
-                console.log(res);
+                if (res.reply) {
+                    addMessageToChat(res.reply, 'response');
+                }
             });
     }
+}
+
+function addMessageToChat(message, side) {
+    const node = document.createElement('li');
+    const textNode = document.createTextNode(message);
+
+    node.classList.add('message');
+
+    if (side === 'client') {
+        node.classList.add('client-message');
+    } else if (side === 'response') {
+        node.classList.add('response-message');
+    }
+
+    node.appendChild(textNode);
+    chatBox.appendChild(node);
 }
