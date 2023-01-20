add37f79a4f6e448c6540f36cdde0ab0325cf22a
Evgeniya Prasolova <prasolova_evgesha@mail.ru>
2023-01-20 21:44:59 +0300

    add tests

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    
+def test_generate_reply():
+    passed_text = 'Hello! What is your name?'
+    response = client.post('/generate-reply/', json={'utterance': passed_text})
+    assert response.status_code == 200
+    assert response.json()['reply'] != ''
+
+def test_generate_reply_empty():
+    passed_text = ''
+    response = client.post('/generate-reply/', json={'utterance': passed_text})
+    assert response.status_code == 200
+    assert response.json()['reply'] != ''
